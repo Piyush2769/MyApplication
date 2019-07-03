40c7db96c8e243b964ad4af4856dea281bfa04dd
Piyush Maheswari <startcheckingyourmail@gmail.com>
2019-07-03 18:43:02 +0530

    Added new.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Button input;
-
+    ImageButton sd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         input=findViewById(R.id.bsq);
+
 
     }
 }
