b91c73dbde68842aa041b88dbdd4c00711cae9d6
Piyush Maheswari <startcheckingyourmail@gmail.com>
2019-07-10 14:09:58 +0530

    Added questions.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,21 +2,55 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button input;
-    ImageButton sd;
+    Button bsimple,btough,bother,brate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input=findViewById(R.id.bsq);
+        bsimple=findViewById(R.id.bsq);
+        btough=findViewById(R.id.tsq);
+        bother=findViewById(R.id.see);
+        brate=findViewById(R.id.rate);
 
+
+        bsimple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,SimpleQuestions.class);
+                startActivity(i);
+            }
+        });
+
+        btough.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,ToughQuestions.class);
+                startActivity(i);
+            }
+        });
+
+        bother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        brate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
